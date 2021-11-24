b79d25195e7334bca8dcf3f2af87a0123990070b
Axel Casella <axel.casella@davinci.edu.ar>
2021-11-24 20:02:14 -0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,74 @@
+let allMusic = [
+  {
+    name: "Gustavo Cerati - Puente",
+    artist: "Bocanada",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Gustavo Cerati - Crimen",
+    artist: "Ahí Vamos",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Gustavo Cerati - Lago en el Cielo",
+    artist: "Ahí Vamos",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Gustavo Cerati - Adiós",
+    artist: "Ahí Vamos",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Gustavo Cerati - Cactus",
+    artist: "Fuerza Natural",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Gustavo Cerati - Deja Vu",
+    artist: "Fuerza Natural",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Gustavo Cerati - Magia",
+    artist: "Fuerza Natural",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Gustavo Cerati - Me Quedo Aquí",
+    artist: "Ahí Vamos",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Gustavo Cerati - Fuerza Natural",
+    artist: "Fuerza Natural",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Gustavo Cerati - Rapto",
+    artist: "Fuerza Natural",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Gustavo Cerati - Karaoke",
+    artist: "Siempre Es Hoy",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Gustavo Cerati - Tu Locura",
+    artist: "Canciones elegidas 93-04",
+    img: "music-12",
+    src: "music-12"
+  },
+];
